de46066e84d15cfbf3023b490c5cf53f5869fbf0
Ryan Roth <ryanmroth@users.noreply.github.com>
2022-11-11 15:39:34 -0500

    Minor formatting updates

--- .config/ohmyzsh/exports.zsh
+++ .config/ohmyzsh/exports.zsh
@@ -15,7 +15,7 @@
 #  --------------------------------------------------
 #   1.  Language & Encoding
 #  --------------------------------------------------
-#
+
     # Prefer US English and use UTF-8
     export LANG='en_US.UTF-8';
     export LC_ALL='en_US.UTF-8';
